9dcd1ec380a58e66d074529947391abe9920e2be
zhuzhe <zhe.zhu1@pactera.com>
2018-06-04 09:19:47 +0800

    初始化工程

---
+++ src/test/java/com/space/aspect/AspectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.space.aspect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AspectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
